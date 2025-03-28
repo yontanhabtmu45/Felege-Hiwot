9ca49e9b7ad566a6553102ff8f327e85bddf3832
Jhan-hab <jhanhab51@gmail.com>
2025-03-28 23:44:37 +0300

    "counter"

--- src/Components/Header/LowerHeader.js
+++ src/Components/Header/LowerHeader.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import classes from './LowerHeader.module.css'
 
 const LowerHeader = () => {
+
+  const [count, setCount] =  useState((25));
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setCount((year) => year + 1)
+    }, 7000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+
+
+
   return (
     <div className={classes.lower_header}>
         <div className={classes.welcome_page}>
             <p>እንኳን በደኅና መጡ !</p>
-            <span><bold>25</bold><sup>ኛ</sup>ዓመት የምስረታ በዓል!!!</span>
+            <span><bold>{count}</bold><sup>ኛ</sup>ዓመት የምስረታ በዓል!!!</span>
             <p>እንኳን አደረሳቹ።</p>
         </div>
     </div>
